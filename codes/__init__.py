562edfdb70cce806b20c5467550d1a4da6b5fc53
Vikram <55603725+vikragha@users.noreply.github.com>
2023-06-15 11:42:24 -0400

    Add files via upload

---
+++ codes/__init__.py
@@ -0,0 +1,25 @@
+"""The qLEET Package for visualizing quantum circuit behavior"""
+import os
+
+import qleet.examples
+import qleet.analyzers
+import qleet.simulators
+import qleet.interface
+
+from qleet.interface.metas import AnalyzerList
+from qleet.interface.circuit import CircuitDescriptor
+
+from qleet.analyzers.training_path import OptimizationPathPlotter
+from qleet.analyzers.loss_landscape import LossLandscapePlotter
+from qleet.analyzers.expressibility import Expressibility
+from qleet.analyzers.entanglement import EntanglementCapability
+from qleet.analyzers.entanglement_spectrum import EntanglementSpectrum
+from qleet.analyzers.histogram import ParameterHistograms
+
+from qleet.simulators.circuit_simulators import CircuitSimulator
+from qleet.simulators.pqc_trainer import PQCSimulatedTrainer
+
+from qleet.examples.qaoa_maxcut import QAOACircuitMaxCut, MaxCutMetric
+from qleet._version import __version__
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
---
+++ codes/_version.py
@@ -0,0 +1,3 @@
+"""Version number (major.minor.patch[-label])"""
+
+__version__ = "0.2.0.2"
