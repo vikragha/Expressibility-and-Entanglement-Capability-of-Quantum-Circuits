86ae555b282d6942ea5bafde57001b125b6d57c8
Vikram <55603725+vikragha@users.noreply.github.com>
2023-06-15 14:26:44 -0400

    Update pqc_trainer.py

--- codes/simulators/pqc_trainer.py
+++ codes/simulators/pqc_trainer.py
@@ -1,17 +1,7 @@
-"""The module which houses the Parametrized Quantum Circuit trainer class.
-
-It generates the TensorFlow Quantum model, and allows Keras like API to
-train and evaluate a model.
-"""
-
 import typing
 import warnings
 
-import cirq
-import tqdm.auto as tqdm
-
 import tensorflow as tf
-import tensorflow_quantum as tfq
 
 from ..interface.metas import AnalyzerList
 from ..interface.circuit import CircuitDescriptor
@@ -21,7 +11,7 @@ warnings.filterwarnings("ignore")
 
 class PQCSimulatedTrainer:
     """A class to train parametrized Quantum Circuits in Tensorflow Quantum
-    Uses gradient descent over the provided parameters, using the TFQ Adjoin differentiator.
+    Uses gradient descent over the provided parameters, using the TFQ Adjoint differentiator.
     """
 
     def __init__(self, circuit: CircuitDescriptor):
@@ -30,14 +20,7 @@ class PQCSimulatedTrainer:
         :param circuit: The circuit object to train on the loss function
         """
         self.optimizer = tf.keras.optimizers.Adam(lr=0.01)
-        self.pqc_layer = tfq.layers.PQC(
-            circuit.cirq_circuit,
-            circuit.cirq_cost,
-            differentiator=tfq.differentiators.Adjoint(),
-        )
-        self.model = tf.keras.models.Sequential(
-            [tf.keras.layers.Input(shape=(), dtype=tf.dtypes.string), self.pqc_layer]
-        )
+        self.model = tf.keras.models.Sequential([circuit.qiskit_circuit])
         self.circuit = circuit
 
     def train(
@@ -51,7 +34,7 @@ class PQCSimulatedTrainer:
         :returns: The trained model
         :rtype: tf.keras.Model
         """
-        dummy_input = tfq.convert_to_tensor([cirq.Circuit()])
+        dummy_input = tf.zeros((1, self.circuit.num_qubits))
         total_error = 0.0
         with tqdm.trange(n_samples) as iterator:
             iterator.set_description("QAOA Optimization Loop")
@@ -59,10 +42,8 @@ class PQCSimulatedTrainer:
                 with tf.GradientTape() as tape:
                     error = self.model(dummy_input)
                 grads = tape.gradient(error, self.model.trainable_variables)
-                self.optimizer.apply_gradients(
-                    zip(grads, self.model.trainable_variables)
-                )
-                error = error.numpy()[0][0]
+                self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+                error = error.numpy()
                 if loggers is not None:
                     loggers.log(self, error)
                 total_error += error
@@ -76,13 +57,13 @@ class PQCSimulatedTrainer:
         :returns: The average loss of the circuit over all the samples
         :rtype: float
         """
-        dummy_input = tfq.convert_to_tensor([cirq.Circuit()])
+        dummy_input = tf.zeros((1, self.circuit.num_qubits))
         total_error = 0.0
         with tqdm.trange(n_samples) as iterator:
             iterator.set_description("QAOA Evaluation Loop")
             for step in iterator:
                 error = self.model(dummy_input)
-                error = error.numpy()[0][0]
+                error = error.numpy()
                 total_error += error
                 iterator.set_postfix(error=total_error / (step + 1))
         return total_error / n_samples
