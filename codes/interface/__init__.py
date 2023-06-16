4eb6d0652c57ba3b5b40dc1059df16653fc21414
Vikram <55603725+vikragha@users.noreply.github.com>
2023-06-15 23:29:50 -0400

    Update __init__.py

--- codes/interface/__init__.py
+++ codes/interface/__init__.py
@@ -1,5 +1,5 @@
-import qleet.interface.circuit
-import qleet.interface.metas
-import qleet.interface.metric_spec
+import codes.interface.circuit
+import codes.interface.metas
+import codes.interface.metric_spec
 
 # import qleet.interface.dashboard  FIXME: Try to get a default import on this
