b379b33cd81a9f0d36d9749e1538d8636b9644f1
Vikram <55603725+vikragha@users.noreply.github.com>
2023-06-21 14:52:28 -0400

    Update test_training_path.py

--- codes/tests/analyzers/test_training_path.py
+++ codes/tests/analyzers/test_training_path.py
@@ -1,22 +1,22 @@
 import networkx as nx
 import plotly.graph_objects as pg
 
-import qleet
+import codes
 
 
 def test_plot_path():
     graph = nx.gnm_random_graph(n=10, m=40)
-    qaoa = qleet.examples.qaoa_maxcut.QAOACircuitMaxCut(graph, p=1)
-    circuit = qleet.interface.circuit.CircuitDescriptor(
+    qaoa = codes.examples.qaoa_maxcut.QAOACircuitMaxCut(graph, p=1)
+    circuit = codes.interface.circuit.CircuitDescriptor(
         qaoa.qaoa_circuit, qaoa.params, qaoa.qaoa_cost
     )
-    trainer = qleet.simulators.pqc_trainer.PQCSimulatedTrainer(circuit)
-    metric = qleet.examples.qaoa_maxcut.MaxCutMetric(graph)
+    trainer = codes.simulators.pqc_trainer.PQCSimulatedTrainer(circuit)
+    metric = codes.examples.qaoa_maxcut.MaxCutMetric(graph)
 
-    plot = qleet.analyzers.loss_landscape.LossLandscapePlotter(trainer, metric, dim=2)
-    trackers = qleet.interface.metas.AnalyzerList(
-        qleet.analyzers.training_path.LossLandscapePathPlotter(plot),
-        qleet.analyzers.training_path.OptimizationPathPlotter(mode="tSNE"),
+    plot = codes.analyzers.loss_landscape.LossLandscapePlotter(trainer, metric, dim=2)
+    trackers = codes.interface.metas.AnalyzerList(
+        codes.analyzers.training_path.LossLandscapePathPlotter(plot),
+        codes.analyzers.training_path.OptimizationPathPlotter(mode="tSNE"),
     )
     for _i in range(5):
         trainer.train(loggers=trackers)
